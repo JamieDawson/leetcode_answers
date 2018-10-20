169d19a995da286bf758e56c2da862cd4177536d
Jamie Dawson <jadawson@e1z3r7p4.42.us.org>
2018-10-20 15:38:14 -0700

    updated with notes

--- toLowerCase/toLowerCase.c
+++ toLowerCase/toLowerCase.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 char* toLowerCase(char* str) {
     int i = 0;
 	while (str[i])
@@ -11,10 +9,27 @@ char* toLowerCase(char* str) {
     return (str);
 }
 
-int		main()
-{
-	char str[6] = "";
-	toLowerCase(str);
-	printf("%s", str);
-	return (0);
-}
+/*
+Example 1:
+Input: "Hello"
+Output: "hello"
+
+Example 2:
+Input: "here"
+Output: "here"
+
+Example 3:
+Input: "LOVELY"
+Output: "lovely"
+
+Explained: str[i] is the index of the string. If we were to pass in "Hello" to str and we typed str[1] it would be at the 'e' spot in the string. "Hello" is represented as 
+str[0] = 'H'
+str[1] = 'e'
+str[2] = 'l'
+str[3] = 'l'
+str[4] = 'o'
+We can't do str[i + 32] because it would skip way past any spot in the string. Which is why we have the Plus-Equals symbol (+=) on the outside of it. The Plus-Equals symbol adds and applies the ascii value to letter.
+Ascii value of 'H' is 72. 72 + 32 = 104. The ascii value of 103 is 'h'.
+
+Type "man ascii" in the command line to see the decimal ascii values. You'll see that += 32 will make all Uppercase letters lowercase.
+*/
